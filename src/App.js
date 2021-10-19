2532961d416fae87ad5bfd9d6419f8d86cc00947
RouberR <Rouber1998@gmail.com>
2021-10-19 20:30:13 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,44 +1,39 @@
-
-import './App.css';
-import MoreOrLess from './more-or-less/MoreOrLess';
-import { Route} from 'react-router-dom';
-import { Link} from 'react-router-dom';
-import Test from './test/Test';
+import "./App.css";
+import MoreOrLess from "./more-or-less/MoreOrLess";
+import { Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Test from "./test/Test";
 
 function App() {
   return (
     <div className="App">
-   
-    <Route path='/moreOrLess' >
-      <MoreOrLess/>
-    </Route>
-    <Route path='/test' >
-      <Test/>
-    </Route>
+      <Route path="/moreOrLess">
+        <MoreOrLess />
+      </Route>
+      <Route path="/test">
+        <Test />
+      </Route>
 
-    <div className="conteinerBox">
-      <div className="box">
-      <h4>Игра больше-меньше</h4>
-      <img src='/moreOrLess.png' />
-      <div>
-      <Link to='/MoreOrLess'>
-      <button   className="buttonBox">Перейти</button>
-      </Link>
-      </div>   
-      </div>
+      <div className="conteinerBox">
+        <div className="box">
+          <h4>Игра больше-меньше</h4>
+          <img src="/moreOrLess.png" />
+          <div>
+            <Link to="/MoreOrLess">
+              <button className="buttonBox">Перейти</button>
+            </Link>
+          </div>
+        </div>
 
-      <div className="box">
-      <h4>Тест</h4>
-      <img  />
-      <div>
-      <Link to='/Test'>
-      <button   className="buttonBox">Перейти</button>
-      </Link>
-      </div>
-      </div>
-
-
-
+        <div className="box">
+          <h4>Тест</h4>
+          <img />
+          <div>
+            <Link to="/Test">
+              <button className="buttonBox">Перейти</button>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/more-or-less/MoreOrLess.jsx
+++ src/more-or-less/MoreOrLess.jsx
@@ -6,7 +6,7 @@ const MoreOrLess = () => {
     const [minRandom, setMinRandom] = React.useState('1');
     const [maxRandom, setMaxRandom] = React.useState('100');
     const [randomNumber, setRandomNumber] = React.useState('');
-   
+
     const onChangeNumber = (event) => {
         setChangeNumber(event.target.value)
     }
@@ -19,32 +19,39 @@ const MoreOrLess = () => {
     const onRandomNumber = () => {
         setRandomNumber(Math.floor(Math.random() * (Math.floor(maxRandom) - Math.ceil(minRandom) + 1)) + Math.ceil(minRandom))
     }
-
     console.log(randomNumber)
+    const defaultNumber = () => {
+        setMinRandom("1")
+        setMaxRandom("100") 
+    }
 
-    
-    const logicGame = () => {if (changeNumber == randomNumber) {return 'Вы победили! Нажмите на новое рандомное число!'}else if (changeNumber > randomNumber){
-        return 'Ваше число больше'} else if (changeNumber < randomNumber){return `Ваше число меньше`} else {return `Введите число!`}}
+
+    const logicGame = () => {
+        if (changeNumber == randomNumber) {
+            return 'Вы победили! Нажмите на новое рандомное число!'
+        } else if (changeNumber > randomNumber) {
+            return 'Ваше число больше'
+        } else if (changeNumber < randomNumber) {
+            return 'Ваше число меньше'
+        } else { return 'Введите число!'}
+    }
 
     return (
         <div className={styles.header}>
             <div className={styles.box}>
-           <div>
-           <h2>{`Игра больше-меньше, отгадай число от ${minRandom} до ${maxRandom}`}</h2>
-           <h2 className={styles.logicText}>{logicGame()}</h2>
-           <h3>{changeNumber ? `Ваше число: ${changeNumber}` : `Ваше число:`}</h3>
-           <input onChange={onChangeNumber} value={Number} value={changeNumber}  placeholder = 'Ваше число'></input>
-           </div>
-           <div>
-           <button onClick={onRandomNumber}  className={styles.textRandom}>Новое рандомное число</button>
-           </div>
-           <div className={styles.buttonRandom}>
-                <p>От <input onChange={onChangeMinRandom} placeholder={minRandom} value={minRandom} ></input></p>
-                <p>До <input onChange={onChangeMaxRandom} placeholder={maxRandom} value={maxRandom}></input></p>
-                 <button className={styles.textRandom} onClick={() => setMinRandom("1") | setMaxRandom("100")}>По умолчанию</button>
-               
-                 </div>
-                
+                <div>
+                    <h2>{`Игра больше-меньше, отгадай число от ${minRandom} до ${maxRandom}`}</h2>
+                    <h2 className={styles.logicText}>{logicGame()}</h2>
+                    <h3>{changeNumber ? `Ваше число: ${changeNumber}` : 'Ваше число:'}</h3>
+                    <input onChange={onChangeNumber} value={changeNumber} placeholder='Ваше число'/>
+                </div>
+
+                <div className={styles.buttonRandom}>
+                    <button onClick={onRandomNumber} className={styles.textRandom}>Новое рандомное число</button>
+                    <p>От <input onChange={onChangeMinRandom} placeholder={minRandom} value={minRandom}/></p>
+                    <p>До <input onChange={onChangeMaxRandom} placeholder={maxRandom} value={maxRandom}/></p>
+                    <button className={styles.textRandom} onClick={defaultNumber}>По умолчанию</button>
+                </div>
             </div>
         </div>
     )
